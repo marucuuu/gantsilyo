925ba2fb0c3db693f790f1bae6ea56dc0300f78b
Marco <mmajcastro@tip.edu.ph>
2024-11-10 19:36:42 +0800

    "updated"

--- gproject/deployment.py
+++ gproject/deployment.py
@@ -22,14 +22,17 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+conn_str = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
+if conn_str:
+    conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': conn_str_params.get('dbname'),
+            'HOST': conn_str_params.get('host'),
+            'USER': conn_str_params.get('user'),
+            'PASSWORD': conn_str_params.get('password'),
+        }
     }
-}
+else:
+    raise EnvironmentError("AZURE_POSTGRESQL_CONNECTIONSTRING not set.")
