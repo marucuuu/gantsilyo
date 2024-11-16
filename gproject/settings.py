1cdb62c3e965bf7fabf97c891c8eaaa6b98665f9
Marco <mmajcastro@tip.edu.ph>
2024-11-16 10:55:15 +0800

    updated code

--- gproject/settings.py
+++ gproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-+z)+5muph0lo8t!c7wfs^0!p9un05(^i1nl^69_98o=iv4)^sy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'gantsilyo.azurewebsites.net'), '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -125,6 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
