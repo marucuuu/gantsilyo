48346177824265f047eecc8986c2a38b8acfcc4f
Marco <mmajcastro@tip.edu.ph>
2024-11-16 18:36:40 +0800

    updated

--- gproject/settings.py
+++ gproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+z)+5muph0lo8t!c7wfs^0!p9un05(^i1nl^69_98o=iv4)^sy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
--- gproject/wsgi.py
+++ gproject/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'gproject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'gproject.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gproject.settings')
 
 application = get_wsgi_application()
